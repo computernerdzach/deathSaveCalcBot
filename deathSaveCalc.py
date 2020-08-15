ace0872ccfcc8756baa0243d9bd7c5b2cdac9277
computernerdzach <wizteczach@gmail.com>
2020-08-15 16:27:52 -0700

    rolls add up to track for one player right now. all players rolls track in the same counter for now

--- deathSaveCalc.py
+++ deathSaveCalc.py
@@ -3,50 +3,58 @@ import random
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-channel1ID = int(os.getenv('CHANNEL1'))
-client = discord.Client()
-successes = 0
-fails = 0
-hit_meter = '[ ][ ][ ]'
-miss_meter = '[ ][ ][ ]'
-
 
 def compare(hits, misses):
     if hits == 0:
         hit_meter = '[ ][ ][ ]'
     if hits == 1:
-        hit_meter = '[*][ ][ ]'
+        hit_meter = '[0][ ][ ]'
     if hits == 2:
-        hit_meter = '[*][*][ ]'
+        hit_meter = '[0][0][ ]'
     if hits == 3:
-        hit_meter = '[*][*][*]'
+        hit_meter = '[0][0][0]'
     if misses == 0:
         miss_meter = '[ ][ ][ ]'
     if misses == 1:
-        miss_meter = '[*][ ][ ]'
+        miss_meter = '[0][ ][ ]'
     if misses == 2:
-        miss_meter = '[*][*][ ]'
+        miss_meter = '[0][0][ ]'
     if misses == 3:
-        miss_meter = '[*][*][*]'
+        miss_meter = '[0][0][0]'
     compare_result = (hit_meter + " Successes\n" + miss_meter + " Failures")
     print(compare_result)
     return compare_result
 
 
-def death_save(hits, misses):
-    death_roll = random.randint(1, 20)
-    print(death_roll)
-    if death_roll == 20:
-        hits += 2
-    elif death_roll in range(10,20):
-        hits += 1
-    elif death_roll == 1:
-        misses += 2
-    elif death_roll in range(2,10):
-        misses += 1
-    compare(hits, misses)
+successes = 0
+fails = 0
+
+
+def death_save():
+    global successes
+    global fails
+    while successes < 3 or fails < 3:
+        death_roll = random.randint(1, 20)
+        print(death_roll)
+        if death_roll == 20:
+            successes += 2
+        elif death_roll in range(10, 20):
+            successes += 1
+        elif death_roll == 1:
+            fails += 2
+        elif death_roll in range(2, 10):
+            fails += 1
+        return str(str(death_roll) + "\n" + compare(successes, fails))
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+channel1ID = int(os.getenv('CHANNEL1'))
+client = discord.Client()
+hits = 0
+misses = 0
+hit_meter = '[ ][ ][ ]'
+miss_meter = '[ ][ ][ ]'
 
 
 @client.event
@@ -62,9 +70,7 @@ async def on_message(message):
         return
 
     if '!death' in message.content.lower():
-        death_save(successes, fails)
-        hit_meter, miss_meter = compare(successes, fails)
-        await message.channel.send()
+        await message.channel.send(death_save())
 
-death_save(successes, fails)
 
+client.run(TOKEN)
